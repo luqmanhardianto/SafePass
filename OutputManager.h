9778284370b60486378684e28768d3c2de4788ff
luqman hardianto <luqmanhardianto@gmail.com>
2026-09-10 11:29:58 +0700

    create:class OutputManager

--- OutputManager.h
+++ OutputManager.h
@@ -1,3 +1,16 @@
 #pragma once
 
 #include <Arduino.h>
+#include "HardwareMap.h"
+#include "Config.h"
+
+class OutputManager {
+public:
+  bool begin();
+
+  void writeOutput(DigitalOutput output, bool state);
+
+  bool readOutput(DigitalOutput output) const;
+private:
+  uint8_t outputState = 0;
+}
