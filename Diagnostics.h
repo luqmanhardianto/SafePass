2358a657da48ac3d1d15e894498c1556a59d1168
luqman hardianto <luqmanhardianto@gmail.com>
2026-09-11 09:01:48 +0700

    create:Diagnostics class header

--- Diagnostics.h
+++ Diagnostics.h
@@ -4,3 +4,17 @@
 
 class InputManager;
 class OutputManager;
+
+class Diagnostics {
+public:
+  void begin(InputManager* inputManager, OutputManager* outputManager);
+  void update();
+private:
+  InputManager* input = nullptr;
+  OutputManager* output = nullptr;
+
+  uint32_t lastStatusTime = 0;
+
+  void showInputState();
+  void shwoOutputState();
+};
