0fd0dec3212e13ae60d3d498f52b1a46b99b7987
luqman hardianto <luqmanhardianto@gmail.com>
2026-09-12 12:24:53 +0700

    create:implementation isInitialized

--- TCA9554.cpp
+++ TCA9554.cpp
@@ -47,4 +47,6 @@ bool TCA9554::writeOutputState(uint8_t outputState) {
 
 bool TCA9554::setOutput(DigitalOutput output, bool state);
 bool TCA9554::getOutput(DigitalOutput output) const;
-bool TCA9554::isInitialized() const;
+bool TCA9554::isInitialized() const{
+  return initialized;
+}
