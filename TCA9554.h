c36849bbc7388a69ab7795a0d7c3edade80c8b63
luqman hardianto <luqmanhardianto@gmail.com>
2026-09-12 11:09:09 +0700

    add:private variable, func. and add public initialized status

--- TCA9554.h
+++ TCA9554.h
@@ -1,11 +1,19 @@
 #pragma once
 
 #include <Arduino.h>
+#include <stdint.h>
+
 #include "HardwareMap.h"
 
 class TCA9554 {
+private:
+  uint8_t sharedOutputState = 0x00;
+  bool initialized = false;
+
+  bool writeOutputState(uint8_t outputState);
 public:
   bool begin();
   bool setOutput(DigitalOutput output, bool state);
   bool getOutput(DigitalOutput output) const;
+  bool isInitialized() const;
 };
