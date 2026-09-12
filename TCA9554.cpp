df61e33540b147794e854e907b5a8329df7bec02
luqman hardianto <luqmanhardianto@gmail.com>
2026-09-12 11:54:18 +0700

    create:writeOutputState

--- TCA9554.cpp
+++ TCA9554.cpp
@@ -1,8 +1,15 @@
+#include "Config.h"
 #include "TCA9554.h"
 #include <Wire.h>
 
 bool TCA9554::begin();
-bool TCA9554::writeOutputState(uint8_t outputState);
+bool TCA9554::writeOutputState(uint8_t outputState) {
+  Wire.beginTransmission(TCA9554_ADDRESS);
+  Wire.write(TCA9554_REG_OUTPUT);
+  Wire.write(outputState);
+  return Wire.endTransmission() == 0;
+}
+
 bool TCA9554::setOutput(DigitalOutput output, bool state);
 bool TCA9554::getOutput(DigitalOutput output) const;
 bool TCA9554::isInitialized() const;
