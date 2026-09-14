703a3bdea43db42d381cbd85463def52981173f5
luqman hardianto <luqmanhardianto@gmail.com>
2026-09-14 15:32:15 +0700

    create:implement Door.lock

--- Door.cpp
+++ Door.cpp
@@ -5,25 +5,31 @@ void Door::begin(
   DoorSensor &doorSensor,
   Lock &lock,
   Indicator &redIndicator,
-  Indicator &greenIndicator){
-    this->pushButton = &pushButton;
-    this->doorSensor=&doorSensor;
-    this->lock=&lock;
-    this->redIndicator=&redIndicator;
-    this->greenIndicator= &greenIndicator;
-  }
+  Indicator &greenIndicator) {
+  this->pushButton = &pushButton;
+  this->doorSensor = &doorSensor;
+  this->lock = &lock;
+  this->redIndicator = &redIndicator;
+  this->greenIndicator = &greenIndicator;
+}
 
-bool Door::isButtonPressed() const{
+bool Door::isButtonPressed() const {
   return pushButton->isPressed();
 }
 
-bool Door::isClosed() const{
+bool Door::isClosed() const {
   return doorSensor->isClosed();
 }
 
-bool Door::isLocked() const{
+bool Door::isLocked() const {
   return lock->isLocked();
 }
-bool Door::isOpen() const{
+bool Door::isOpen() const {
   return !doorSensor->isClosed();
 }
+
+bool Door::lock(){
+  return lock->lock();
+}
+
+bool Door::unlock();
