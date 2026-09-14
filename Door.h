2662050daebe8afcf01fca51945f881fb9e1f2da
luqman hardianto <luqmanhardianto@gmail.com>
2026-09-14 13:42:29 +0700

    create:interface Doorr

---
+++ Door.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Arduino.h>
+
+#include "PushButton.h"
+#include "DoorSensor.h"
+#include "Lock.h"
+#include "Indicator.h"
+
+class Door {
+private:
+  PushButton *pushButton;
+  DoorSensor *doorSensor;
+  Lock *lock;
+  Indicator *redIndicator;
+  Indicator *greenIndicator;
+public:
+  void begin(
+    PushButton &pushButton,
+    DoorSensor &doorSensor,
+    Lock &lock,
+    Indicator &redIndicator,
+    Indicator &greenIndicator);
+
+  bool isButtonPressed() const;
+  bool isClosed() const;
+  bool isLocked() const;
+  bool isOpen() const;
+};
