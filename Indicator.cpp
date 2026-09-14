76d33502c8e2a62e43d230498c9b582700df8e68
luqman hardianto <luqmanhardianto@gmail.com>
2026-09-14 10:25:43 +0700

    create:implementation Indicator.begin

--- Indicator.cpp
+++ Indicator.cpp
@@ -1,6 +1,15 @@
 #include "Indicator.h"
 
-bool Indicator::begin(TCA9554 &tca9554, DigitalOutput output);
+bool Indicator::begin(TCA9554 &tca9554, DigitalOutput output) {
+  this->tca9554 = &tca9554;
+  this->output = output;
+  this->onState = false;
+
+  if (!this->tca9554->isInitialized()) {
+    return false;
+  }
+  return true;
+}
 bool Indicator::on();
 bool Indicator::off();
 bool Indicator::isOn() const;
