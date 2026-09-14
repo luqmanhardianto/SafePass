c4d98a1f42a51d2018a25b9c675ac4aa7e1612eb
luqman hardianto <luqmanhardianto@gmail.com>
2026-09-14 09:49:51 +0700

    test:Lock implement TCA9554

--- SafePass.ino
+++ SafePass.ino
@@ -7,11 +7,12 @@
 // define Input object DoorA
 PushButton pushButtonA;
 DoorSensor doorSensorA;
+Lock lockA;
 
 // define Input object DoorB
 PushButton pushButtonB;
 DoorSensor doorSensorB;
-
+Lock lockB;
 
 // define TCA9554
 TCA9554 tca9554;
@@ -19,13 +20,6 @@ TCA9554 tca9554;
 void setup() {
   Serial.begin(115200);
   delay(500);
-  // Init digital inputs.
-  pushButtonA.begin(DigitalInput::DI1);
-  doorSensorA.begin(DigitalInput::DI2);
-
-  pushButtonB.begin(DigitalInput::DI5);
-  doorSensorB.begin(DigitalInput::DI6);
-
   // init tca9554
   if (tca9554.begin()) {
     Serial.println("tca9554 ok");
@@ -33,9 +27,32 @@ void setup() {
     Serial.println("tca9554 error");
   }
 
+  // Init digital inputs.
+  pushButtonA.begin(DigitalInput::DI1);
+  doorSensorA.begin(DigitalInput::DI2);
+
+  pushButtonB.begin(DigitalInput::DI5);
+  doorSensorB.begin(DigitalInput::DI6);
+
+  // init digital output
+  lockA.begin(tca9554, DigitalOutput::DO1);
+  lockB.begin(tca9554, DigitalOutput::DO5);
+
   Serial.println("SafePass Lock/TCA9554 test started");
 }
 
 void loop() {
+  pushButtonA.readState();
+  if (pushButtonA.isPressed()) {
+    lockA.lock();
+  } else {
+    lockA.unlock();
+  }
 
+  pushButtonB.readState();
+  if (pushButtonB.isPressed()) {
+    lockB.lock();
+  } else {
+    lockB.unlock();
+  }
 }
