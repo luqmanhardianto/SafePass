05b571fe8596c0ad36a89baafb125d0c7d92b6aa
luqman hardianto <luqmanhardianto@gmail.com>
2026-09-14 15:12:27 +0700

    create:implement Interlock.begin

--- InterLock.cpp
+++ InterLock.cpp
@@ -1,7 +1,11 @@
 #include "InterLock.h"
 #include "Config.h"
 
-void Interlock::begin(Door &doorA, Door &doorB);
+void Interlock::begin(Door &doorA, Door &doorB) {
+  this->doorA = &doorA;
+  this->doorB = &doorB;
+  state = State::IDLE;
+}
 
 void Interlock::update();
 Interlock::State Interlock::getState() const;
